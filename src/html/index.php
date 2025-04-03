55889a455d766bf715bcce99f7a3b6ff2a305163
takeshitayy <takeshitayy.w@gmail.com>
2025-04-04 03:16:21 +0900

    index.php の修正

--- src/html/index.php
+++ src/html/index.php
@@ -1,4 +1,14 @@
 
-<p>PHPバージョン: <?php echo phpversion();?></p>
-<p><a href="/.mail-test/send_mail_sendmail.php">mail test by sendmail</a></p>
-<p><a href="/.mail-test/send_mail_smtp.php">mail test by smtp</a></p>
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mail test</title>
+</head>
+<body>
+    <p>PHPバージョン: <?php echo phpversion();?></p>
+    <p><a href="/.mail-test/send_mail_sendmail.php">mail test by sendmail</a></p>
+    <p><a href="/.mail-test/send_mail_smtp.php">mail test by smtp</a></p>
+</body>
+</html>
